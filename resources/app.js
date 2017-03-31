01095573a73ed73bf8234570c9b48a8561f0e91f
betping <betping1993@gmail.com>
2017-03-31 15:20:11 +0530

    Add files via upload

---
+++ resources/app.js
@@ -0,0 +1,9 @@
+var name="betping dak";
+var age=24;
+var dateOfBirth=new Date("march 1, 1993")
+var placeOfBirth="kortom";
+
+console.log("Name="+name);
+console.log("Age="+age);
+console.log("Date of Birth="+dateOfBirth);
+console.log("Place of Birth="+placeOfBirth);
